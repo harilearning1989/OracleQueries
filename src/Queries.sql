7ddc4171570c5873a15c6866100867887855c065
harilearning1989 <59171886+harilearning1989@users.noreply.github.com>
2023-08-11 09:30:27 +0530

    Update Queries.sql

--- src/Queries.sql
+++ src/Queries.sql
@@ -1,2 +1,10 @@
 CREATE TABLE ORACLE_DEMO.EMPLOYEE_MANAGER
 AS SELECT * FROM sys.employees;
+
+SELECT emp.employee_id,emp.last_name emp_name,mngr.employee_id mngr_id,mngr.last_name manager_name 
+FROM TEST_EMPLOYEES emp,TEST_EMPLOYEES mngr
+WHERE emp.manager_id = mngr.employee_id;
+
+SELECT emp.employee_id,emp.last_name emp_name,mngr.employee_id mngr_id,mngr.last_name manager_name 
+FROM TEST_EMPLOYEES emp LEFT JOIN TEST_EMPLOYEES mngr
+ON emp.manager_id = mngr.employee_id ORDER BY manager_name NULLS FIRST ;
